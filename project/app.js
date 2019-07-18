673c31a424bf5d4487207e1bd5c7415a7cab55d9
Aa-code <Aliyahdevcode@outlook.com>
2019-07-18 15:37:34 -0400

    yay

--- project/app.js
+++ project/app.js
@@ -3,45 +3,47 @@ $(() => {
     console.log(data.results[0].name);
     console.log(data);
 
-    const $img = data.results[0].image;
-    const $img1 = $('<img>').attr('src', $img);
-    $img1.addClass('img');
-    $('.results').append($img1);
+    const $imgid = data.results[0].image;
+    const $img1 = $('<img>').attr('src', $imgid);
+    $img1.addClass('imgid');
+    $('.card').append($img1);
 
     const $name = data.results[0].name;
     const $Character = $('<h1>').text(`Name: ${$name}`);
     $Character.addClass('characterName');
-    $('.results').append($Character);
+    $('.card').append($Character);
 
     const $gender = data.results[0].gender;
     const $gender1 = $('<h1>').text(`Gender: ${$gender}`);
     $gender1.addClass('gender');
-    $('.results').append($gender1);
+    $('.card').append($gender1);
 
     const $episode = data.results[0].episode[0];
     const $episode1 = $('<h1>').text(`Found here!:  ${$episode}`);
     $episode1.addClass('episode');
-    $('.results').append($episode1);
+    $('.card').append($episode1);
 
     const $kind = data.results[0].species;
     const $kind1 = $('<h1>').text(`Species: ${$kind}`);
     $kind1.addClass('kind');
-    $('.results').append($kind1);
+    $('.card').append($kind1);
 
     const $status = data.results[0].status;
     const $status1 = $('<h1>').text(`Status: ${$status}`);
     $status1.addClass('status');
-    $('.results').append($status1);
+    $('.card').append($status1);
 
     const $location = data.results[0].location.name;
     const $location1 = $('<h1>').text(`Location: ${$location}`);
     $location1.addClass('location');
-    $('.results').append($location1);
+    $('.card').append($location1);
+
+    $('.card').css('border', '3px solid green');
   };
 
   $('form').on('submit', event => {
     event.preventDefault();
-    $('.results').empty();
+    $('.card').empty();
 
     const $input = $('.title-query').val();
 
@@ -50,6 +52,17 @@ $(() => {
 
     $.ajax({ url: endpoint }).then(handleDate);
   });
+
+  $('#myBtn').on('click', event => {
+    event.preventDefault();
+    $('#myModal').css('display', 'block');
+  });
+
+  $('.close').on('click', event => {
+    $('#myModal').css('display', 'none');
+  });
+
+  // event listener for modal here
 });
 // console.log(endpoint);
 
